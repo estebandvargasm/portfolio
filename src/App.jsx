4dee134a9ed7f740bce104da7845b488b5fc826a
estebandvargasm <estebandvargasm@outlook.com>
2025-10-27 22:01:56 -0500

    feat: add BackgroundRippleEffect and FloatingDock components with demo integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { ThreeDCardDemo } from './test.jsx'
+import { FloatingDockDemo } from './navbar/navbar.jsx'
+import { BackgroundRippleEffectDemo } from './background/background.jsx'
 import './index.css'
 
 function App() {
   return (
     <main className='text-4xl font-bold'>
+      <BackgroundRippleEffectDemo />
+      <FloatingDockDemo />
       <ThreeDCardDemo />
     </main>
   )
--- src/utils/cn.jsx
+++ src/utils/cn.jsx
@@ -1,7 +1,6 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// Utilidad para combinar clases de Tailwind y clases condicionales
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
